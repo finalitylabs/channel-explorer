a517227a8a65e57674268c7ea5f93665e52ea466
Will Vatcher <will.vatcher@gmail.com>
2018-05-18 21:50:15 -0700

    Eth Component

--- src/components/Eth.tsx
+++ src/components/Eth.tsx
@@ -6,7 +6,30 @@ class Eth extends React.Component {
     public render() {
         return (
             <div className='eth-comp'>
-                ETH!
+                <select>
+                    <option selected={true} disabled={true}>Agreement</option>
+                    <option>1</option>
+                    <option>2</option>
+                </select>
+                <div className='eth-select-button'>
+                <select>
+                    <option selected={true} disabled={true}>Channel</option>
+                    <option>1</option>
+                    <option>2</option>
+                </select>
+                
+                    <button>+</button>
+                </div>
+                <div className='eth-send'>
+                    <div className='eth-send-state'>
+                        <h3>State</h3>
+                        <p>XX ETH</p>
+                    </div>
+                    <label>Amount:
+                        <input type='text' />
+                    </label>
+                    <button>Send</button>
+                </div>
             </div>
         );
     }
